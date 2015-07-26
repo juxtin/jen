05b91dad399052e165258a82e46d7320ec4a7200
Justin Holguin <justin.h.holguin@gmail.com>
2015-07-25 21:39:42 -0700

    Put most dependencies in :dev profile

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject jen "0.1.0-SNAPSHOT"
   :description "Schema-style generators for test.check"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/holguinj/jen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/algo.generic "0.1.2"]
-                 [org.clojure/test.check "0.7.0"]
-                 [prismatic/schema "0.4.3"]])
+  :dependencies [[org.clojure/algo.generic "0.1.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
+                                  [prismatic/schema "0.4.3"]
+                                  [org.clojure/test.check "0.7.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
