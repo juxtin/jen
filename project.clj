3d814c553b305f23dd0e437e361dda7cae9ebc90
Justin Holguin <justin.h.holguin@gmail.com>
2015-07-26 17:46:14 -0700

    Update version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jen "0.1.0-SNAPSHOT"
+(defproject jen "0.1.1"
   :description "Schema-style generators for test.check"
   :url "https://github.com/holguinj/jen"
   :license {:name "Eclipse Public License"
